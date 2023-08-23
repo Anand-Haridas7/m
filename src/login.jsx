399586176a4417b3dda8af8eb43646843adee2f3
Anand <anandharidas8921@gmail.com>
2023-08-23 19:58:11 +0530

    folder added

---
+++ src/login.jsx
@@ -0,0 +1,23 @@
+export const Login = () => {
+    return (
+        
+
+        <form action="">
+            <label htmlFor="email">email</label>
+            <input type="email" placeholder="youremail@gmail.com" id="email " name="email" /><br />
+            <label htmlFor="password">password</label>
+
+            <input type="password" placeholder="************" id="password" name="password" />
+
+
+
+
+
+
+        </form>
+    )
+
+
+
+
+}
